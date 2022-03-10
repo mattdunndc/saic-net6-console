7365b815acfd6d64f5a859b538d4e0e1e3bbe040
Matt Dunn <mattdunndc@gmail.com>
2022-03-10 09:53:35 -0500

    Add project files.

---
+++ saic-net6-console/APIModels.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Net6api.Models
+{
+    public class APIModels
+    {
+        //public record User {([Required] int userId, [Required] string email, string firstName, string lastName);
+
+        public class people 
+        {
+            public List<user>? users { get; set; }
+        }
+        public class user
+        {
+            [Required]
+            public int userId { get; set; }
+            [Required]
+            public string? email { get; set; }
+            public string? firstName { get; set; }
+            public string? lastName { get; set; }
+
+            public List<story>? stories { get; set; }
+
+        };
+
+        //public record Story([Required] int storyId, [Required] string title, int userId);
+        public class story
+        {
+            [Required]
+            public int storyId { get; set; }
+            [Required]
+            public string? title { get; set; }      
+            public int? points { get; set; }
+            public DateTime dueDate { get; set; }            
+            [Required]
+            public int userId { get; set; }                 
+        }
+    }
+}
---
+++ saic-net6-console/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var peeps = JsonSerializer.Deserialize<Net6api.Models.APIModels.people>(File.ReadAllText("users-list.json"));
+
+var x = 0;
+
+
+//User: Bob has 0 stories.
+
+//User: Carla has 2 stories.
+//Story ID:8 is 8 points and due: 3 / 20 / 2022
+//Story ID:3 is 3 points and due: 3 / 24 / 2022
+
+//User: Dave has 2 stories.
+//Story ID:2 is 5 points and due: 3 / 17 / 2022
+//Story ID:1 is 3 points and due: 3 / 19 / 2022
+
+//User: Emma has 3 stories.
+//Story ID:6 is 5 points and due: 3 / 13 / 2022
+//Story ID:4 is 5 points and due: 3 / 21 / 2022
+//Story ID:9 is 2 points and due: 4 / 9 / 2022
+
+//User: Jenny has 2 stories.
+//Story ID:5 is 8 points and due: 3 / 20 / 2022
+//Story ID:7 is 8 points and due: 3 / 25 / 2022
+
+//User: Tony has 1 stories.
+//Story ID:10 is 8 points and due: 3 / 18 / 2022
